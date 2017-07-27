52ea27eaedb376a4bee7f3c1b919c29fcc07a1c0
Amit Chanchal <amit.chanchal@hotmail.com>
2017-07-27 23:10:45 +0530

    Pointer

--- basic/5_pointer.cpp
+++ basic/5_pointer.cpp
@@ -5,14 +5,18 @@ using namespace std;
 int main (){
     int a = 10;
     /* 
-    p is a pointer which point to a memory location of size int, which is delared by datatype* <pointerVarName>, here (int* p)
-    To Access address of a variable we use '&' operator and when we use '&a', it means we are asking for address in memory where
-    value associated with variable 'a' is stored.
+        p is a pointer which point to a memory location of size int, which is delared by datatype* <pointerVarName>, here (int* p)
+        To Access address of a variable we use '&' operator and when we use '&a', it means we are asking for address in memory where
+        value associated with variable 'a' is stored.
+        We assign the address of a to pointer p
+        To get value at any address we use *<pointerVar>
 
-    We assign the address of a to pointer p
+        Warning!: 
+        we cannot use multiple declarations with pointers as with other datatypes
+        int a, b, c;  // this works
+        int* a, b,c; // This would translate to int*a, int b, int c;
 
-    To get value at any address we use *<pointerVar>
-     */
+    */
     int* p = &a;
     cout<< "address of a: "<<&a<<endl;
     cout<<" Value at &a: "<< a<<endl;
