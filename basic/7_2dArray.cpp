2a0ddef9e27d49d2503007a8b18fc0f576c29d27
Amit Chanchal <amit.chanchal@hotmail.com>
2017-07-28 12:17:45 +0530

    more arrays

--- basic/7_2dArray.cpp
+++ basic/7_2dArray.cpp
@@ -10,7 +10,8 @@
 using namespace std;
 
 int main(){
-  const char * courses[] = {math, physics, chemistry, biology, english};
+  string  courses[5] = {"math", "physics", "chemistry", "biology", "english"};
+  int totalClassMarks[5] = {0,0,0,0,0};
   int results[5][5] = {
     {12, 8, 11, 9 , 10},
     {10, 13, 15, 14, 11},
@@ -18,11 +19,24 @@ int main(){
     {12, 13, 11, 11, 14},
     {3, 4, 8, 10, 11}
   };
-  for (int i = 0 ; i < 5; i++){
-    cout<<"Scores for student: "<< i<<endl;
-    for (int j = 0; j < 5; j++){
-       cout<<courses[i]<<": "<< results[i][j]<<endl;
+  // We want to add totals for each subject and store it in the totalClassMarks Array
+  for (int i =0; i<5; i++){
+    for(int j = 0; j<5; j++){
+      totalClassMarks[j] +=results[i][j];
     }
+  }
+
+  for (int i =0; i<5; i++){
+    cout<<"- - - - - - --  ScoreCard for student "<<i<<" - - - - - - - - -- - - "<<endl;
+    for(int j = 0; j<5; j++){
+      cout<<courses[j]<<" :"<< results[i][j];
+      if ( results[i][j] >= (totalClassMarks[j]/5) ){
+        cout<<" <Above average>"<<endl;
+      } else {
+        cout<<" <Below average>"<<endl;
+      }
+    }
+
   }
   return 0;
 }
