45578b8df4cf7491f82b406f2b8e5f5ab90406c4
Amit Chanchal <amit.chanchal@hotmail.com>
2017-08-02 14:13:52 +0530

    class update

---
+++ stl/strings.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+    s.find(p) s.find(p, i)
+    s.substr(i,m)
+    s.insert(i, p) s.erase(i, m) s.replace(i, m, p)
+    getline(is, s)
+    Return the index of first occurrence of string p in s Return the index of first occurrence of string p in s
+    on or after position i
+    Return the substring starting at position i of s
+    and consisting of m characters
+    Insert string p just prior to index i in s
+    Remove the substring of length m starting at index i
+    Replace the substring of length m starting at index i with p
+    Read a single line from the input stream is and store the result in s
+*/
